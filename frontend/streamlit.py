a1cf2b160e93aa7dc855c7c310044d2946059e12
OkemaPaulMark <paulokema342@gmail.com>
2025-06-04 15:42:12 +0300

    Initial commit for the module 1 assignment

---
+++ frontend/streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import requests
+
+# response = requests.get("http://backend:8000/")  # Docker uses service name as hostname
+
+st.title("Streamlit + FastAPI")
+# st.write("Backend says:", response.json())
